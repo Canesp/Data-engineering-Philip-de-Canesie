db6979d481ac604fdeb4dc35dfd74e5ecfc35275
Canesp <philip.decanesie@gmail.com>
2023-06-01 11:21:58 +0200

    added date time to file names

--- Code-along/Lec3_airflow_intro/dags/bash_operator.py
+++ Code-along/Lec3_airflow_intro/dags/bash_operator.py
@@ -11,11 +11,16 @@ data_warehouse_path = Path(__file__).parents[1] / "data" / "data_warehouse"
 
 print(data_lake_path)
 
+time_variable = "$(date +%y%m%d_%H%M%S)"
+
 with DAG(dag_id="joke_DAG", start_date=datetime(2023, 6, 1)):
-    say_hello = BashOperator(task_id="say_helllo", bash_command="echo 'Hej HEj!'")
+
+    say_hello = BashOperator(task_id="say_hello", bash_command="echo 'Hej HEj!'")
 
     setup_folders = BashOperator(task_id = "setup_folders", bash_command=f"mkdir -p {data_lake_path} {data_warehouse_path}")
 
-    say_hello >> setup_folders
+    download_joke = BashOperator(task_id="random_joke", bash_command=f"curl -o {data_lake_path}/joke{time_variable}.json https://official-joke-api.appspot.com/random_joke")
+
+    say_hello >> setup_folders >> download_joke
 
 
