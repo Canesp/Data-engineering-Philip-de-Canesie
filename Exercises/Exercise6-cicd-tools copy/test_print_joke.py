a326e5beadc8bfa1fea89bc4251d0a09cb7295b1
Canesp <philip.decanesie@gmail.com>
2023-08-18 12:07:56 +0200

    add tests

--- Exercises/Exercise6-cicd-tools copy/test_print_joke.py
+++ Exercises/Exercise6-cicd-tools copy/test_print_joke.py
@@ -1,15 +1,36 @@
+import sys
+
 from print_joke import get_random_reaction
 
 
 def test_get_random_reaction_type():
+    # gets a random reaction.
     reaction = get_random_reaction()
-    assert False # Replace False with a check that makes sure the reaction is a string type
+    # checks if reaction is a str.
+    assert type(reaction) == str
 
 
 def test_get_random_reaction_repeats():
-    # Write a test that checks that multiple calls to get_random_reaction()
-    # doesn't give you the same reaction every time
-    pass 
+    # test variables.
+    previous_reaction = get_random_reaction()
+    current_reaction = ""
+    num_trys = 0  # curren num of trys.
+    max_trys = 50  # max num of trys.
+
+    # test to see that the reaction is not always the same.
+    while previous_reaction != current_reaction:
+        # gets a new reaction.
+        current_reaction = get_random_reaction()
+        num_trys += 1  # adds one to num of trys
+
+        # if num trys == 50 returns False
+        if num_trys == max_trys:
+            assert False
+
+    # retruns True if success.
+    assert True
 
 
-# Come up with a test of your own and implement it here.
+def test_pyjokes_installed():
+    # checks if pyjoke is installed.
+    assert "pyjokes" in sys.modules
